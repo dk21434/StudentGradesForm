cb73cbd628a4a83d2eec723a3be6a17877287cbd
dk21434 <dimitriskoufakis9@gmail.com>
2023-04-11 22:19:00 +0300

    READYFORM

--- WindowsFormsApp1.0/Form1.Designer.cs
+++ WindowsFormsApp1.0/Form1.Designer.cs
@@ -39,11 +39,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 44);
+            this.button2.Location = new System.Drawing.Point(12, 40);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(162, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "LOAD DATA";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -56,12 +56,12 @@
             this.Student_ID,
             this.StudentName,
             this.AverageGrade});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 85);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 82);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(413, 218);
+            this.dataGridView1.Size = new System.Drawing.Size(428, 227);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -94,11 +94,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 340);
+            this.button3.Location = new System.Drawing.Point(12, 328);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(162, 23);
             this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
+            this.button3.Text = "EXPORT DATA";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
--- WindowsFormsApp1.0/Form1.cs
+++ WindowsFormsApp1.0/Form1.cs
@@ -27,25 +27,25 @@ namespace WindowsFormsApp1._0
         }
 
        
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e) // Button click event handler to display the average grades in a data grid view
         {
-            System.Data.DataTable dt = new System.Data.DataTable();
+            System.Data.DataTable dt = new System.Data.DataTable(); // Call the web service method to get the average grades
             dt = ws.GetAverage();
-            dataGridView1.DataSource = dt;
+            dataGridView1.DataSource = dt;  
 
         }
 
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)  // Button click event handler to export the data grid view to Microsoft Excel
         {
-            System.Data.DataTable dt = new System.Data.DataTable();
+            System.Data.DataTable dt = new System.Data.DataTable(); // Call the web service method to get the average grades
             dt = ws.GetAverage();
 
-            CopyDataGridViewToClipboard(dataGridView1);
+            CopyDataGridViewToClipboard(dataGridView1); // Copy the data grid view to the clipboard for use in Excel
 
 
 
-            Microsoft.Office.Interop.Excel.Application xls = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Application xls = new Microsoft.Office.Interop.Excel.Application(); // Create a new instance of Microsoft Excel and add a new workbook
             Microsoft.Office.Interop.Excel.Workbook xlwb;
             Microsoft.Office.Interop.Excel.Worksheet xlws;
             Microsoft.Office.Interop.Excel.Range xlr;
@@ -55,13 +55,13 @@ namespace WindowsFormsApp1._0
             xlws = xlwb.Worksheets.get_Item(1);
             xlr = xlws.Cells[1, 1];
             xlr.Select();
-            xlws.PasteSpecial(xlr, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+            xlws.PasteSpecial(xlr, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true); // Paste the clipboard data into Excel
             xls.Columns.AutoFit();
             xls.Visible = true;
 
         }
 
-        private void CopyDataGridViewToClipboard(DataGridView dataGridView)
+        private void CopyDataGridViewToClipboard(DataGridView dataGridView) // Utility method to copy the data grid view to the clipboard
         {
             dataGridView.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
             dataGridView.SelectAll();
