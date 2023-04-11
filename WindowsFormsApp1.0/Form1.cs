53ccf14a7821f577c7a5bbdd593bf2e4d07927b0
dk21434 <dimitriskoufakis9@gmail.com>
2023-04-11 20:03:46 +0300

    excel

--- WindowsFormsApp1.0/Form1.cs
+++ WindowsFormsApp1.0/Form1.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.Office.Interop.Excel;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.ComponentModel;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -29,30 +28,28 @@ namespace WindowsFormsApp1._0
        
         private void button2_Click(object sender, EventArgs e)
         {
-            System.Data.DataTable dt = new System.Data.DataTable();
+            DataTable dt = new DataTable();
             dt = ws.GetAverage();
             dataGridView1.DataSource = dt;
+
         }
 
-        
+
         private void button3_Click(object sender, EventArgs e)
         {
-            System.Data.DataTable dt = new System.Data.DataTable();
+            DataTable dt = new DataTable();
             dt = ws.GetAverage();
-            dataGridView1.SelectAll();
-           
-            DataObject obj = dataGridView1.GetClipboardContent();
-            Clipboard.SetDataObject(obj);
+
+            CopyDataGridViewToClipboard(dataGridView1);
+
 
 
             Microsoft.Office.Interop.Excel.Application xls = new Microsoft.Office.Interop.Excel.Application();
             Microsoft.Office.Interop.Excel.Workbook xlwb;
             Microsoft.Office.Interop.Excel.Worksheet xlws;
             Microsoft.Office.Interop.Excel.Range xlr;
-
             Object mv = System.Reflection.Missing.Value;
 
-            
             xlwb = xls.Workbooks.Add(mv);
             xlws = xlwb.Worksheets.get_Item(1);
             xlr = xlws.Cells[1, 1];
@@ -61,11 +58,23 @@ namespace WindowsFormsApp1._0
             xls.Columns.AutoFit();
             xls.Visible = true;
 
-
-
-
         }
 
-       
+        private void CopyDataGridViewToClipboard(DataGridView dataGridView)
+        {
+            dataGridView.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            dataGridView.SelectAll();
+
+            DataObject dataObj = dataGridView.GetClipboardContent();
+            if (dataObj != null)
+            {
+                Clipboard.SetDataObject(dataObj);
+            }
+
+            // Clear the selection if you don't want the cells to remain selected
+            dataGridView.ClearSelection();
+        }
+
+
     }
 }
